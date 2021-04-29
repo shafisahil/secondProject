d7e65e3d734d5b27b61f8ad251d50e389668fe6a
shafisahil <shafisahil7868@gamil.com>
2021-04-29 13:02:51 -0700

    changes

---
+++ basic.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+  <html>
+      <head>
+          <title>code4mk.org</title>
+      </head>
+      <body>
+
+          <script type="text/javascript">
+          let name = 'kamal';
+
+          </script>
+
+      </body>
+  </html>
